1d792dccc177c59ed1efa88add9fcc94fd26a513
Rima Mahmoudi <40065092+vivosalvador@users.noreply.github.com>
2021-03-01 17:54:11 +0100

    First Commit

---
+++ track.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+import matplotlib as plt
+
+path = "Dataset/"
+
+# - extraction d'objets en mouvement (par soustraction de fond).
+
+def ExtractObjects(Frame):
+    # background substraction
+    pass
+
+
+
+
+# - extraction de descripteurs des objets.
+# - suivi des objets par appareillement (matching).
